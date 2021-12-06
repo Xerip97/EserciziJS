045c57c06b44dd46e575e4711171e7c166fa7033
Fabio Viola <fabioviola97@gmail.com>
2021-12-06 16:04:30 +0100

    Fix ex 25

--- 25/exercise-25.js
+++ 25/exercise-25.js
@@ -1,28 +1,28 @@
 const person = {
   firstName: null,
   lastName: null,
-  setFirstName(firstName) {
+  set setFirstName(firstName) {
     this.firstName = firstName;
   },
-  getFirstName() {
+  get getFirstName() {
     return this.firstName;
   },
-  setLastName(lastName) {
+  set setLastName(lastName) {
     this.lastName = lastName;
   },
-  getLastName() {
+  get getLastName() {
     return this.lastName;
   },
   fullName() {
-    return `${this.getFirstName()} ${this.getLastName()}`;
+    return `${this.firstName} ${this.lastName}`;
   }
 }
 
 const john = Object.assign({}, person);
 const simon = Object.assign({}, person);
-john.setFirstName('John');
-john.setLastName('Doe');
-simon.setFirstName('Simon');
-simon.setLastName('Collins');
+john.firstName = 'John';
+john.lastName = 'Doe';
+simon.firstName = 'Simon';
+simon.lastName = 'Collins';
 console.log(john.fullName()); // John Doe
 console.log(simon.fullName()); // Simon Collins
